18eae3bf6b6fa8128dc1192d5ec38bb244709013
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-03-29 19:30:48 +0530

    add apikey validation

--- apps/api/src/controllers/email.controller.ts
+++ apps/api/src/controllers/email.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { SendEmailSchema } from "../types";
 import { producer, TOPIC_EMAIL } from "@repo/kafka";
+import { isApiKeyValid } from "../helper";
+import { Prisma } from "@repo/db";
 
 const sendEmail = async(req: Request, res: Response) => {
     try {
@@ -9,18 +11,43 @@ const sendEmail = async(req: Request, res: Response) => {
         if(!parsedData.success) {
             res.status(400).json({message: parsedData?.error?.issues[0]?.message ?? "Invalid Input"});
             return;
-        }
-        console.log(parsedData.data)
+        };
+        
+        const apiKey = req.headers['x-api-key'];
+        if(!apiKey) {
+            res.status(401).json({message: "Missing Api Key"});
+            return;
+        };
+
+        try {
+            await isApiKeyValid(apiKey as string);
+        } catch (error) {
+            if(error instanceof Error){
+                res.status(500).json({ message : error?.message });
+                return;
+            } else {
+                res.status(500).json({ message : "Something went wrong"});
+                return;
+            }
+        };
+
         await producer.send({
             topic: TOPIC_EMAIL,
             messages: [{
                 value : JSON.stringify(parsedData.data),
             }]
         });
-        res.status(200).json({message : "Email processing..."})
-        console.log('Produced successfully!');
+        res.status(200).json({message : "Email processing..."});
     } catch (error) {
-        console.log("Error occured while sending email!",error);
+        if(error instanceof Prisma.PrismaClientUnknownRequestError) {
+            res.status(500).json({ message : error.message}) 
+            return;
+        }
+        if(error instanceof Error) {
+            res.status(500).json({ message : error.message || "Something went wrong"});
+            return;
+        }
+        res.status(500).json({ message : "Something went wrong"});
     }
 }
 
