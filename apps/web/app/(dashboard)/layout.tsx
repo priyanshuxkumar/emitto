6525cd0dbe3d9f1cfdfacde65839ebef229288a3
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-01 00:18:47 +0530

    refactor custom layout

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,27 +1,20 @@
+import Feedback from "@/components/Feedback";
 import Sidebar from "@/components/Sidebar";
-import { Button } from "@/components/ui/button";
 
-
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
       {/* Sidebar */}
       <nav>
         <aside className="w-64 flex flex-col justify-between h-screen border-r border-slate-6 p-4">
-            <Sidebar />
+          <Sidebar />
         </aside>
       </nav>
 
       {/* Main Content */}
       <main className="w-full">
         <nav className="border-b h-15 border-slate-6 flex items-center justify-end px-6">
-            <Button variant={"secondary"}>
-              Feedback
-            </Button>
+          <Feedback/>
         </nav>
         {children}
       </main>
