25cf96f1341e9608c4b714ee6aa7ece0b0e815cf
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-03-31 17:17:12 +0530

    add helper function for time

---
+++ apps/web/helper/time.ts
@@ -0,0 +1,10 @@
+import { formatDistanceToNow } from "date-fns";
+
+export function timeAgo(timestamp: Date) {
+    if(timestamp) {
+        return formatDistanceToNow(new Date(timestamp), { addSuffix: true });
+    } else {
+        return null
+    }
+}
+
