1360fadbb59832661664cb809b966a9b92c2894c
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-05 01:10:06 +0530

    add keys

--- apps/api/src/services/redis/keys.ts
+++ apps/api/src/services/redis/keys.ts
@@ -1,19 +1,40 @@
 /** Users specific keys */
-const getLoggedInUserKey = (userId : number) : string => {
-    return (`loggedInUser:${userId}`);
-}
+const getLoggedInUserKey = (userId: number): string => {
+  return `logged-in-user:${userId}`;
+};
 
 /** Emails specific keys */
-const getAllEmailsKey = (userId : number) : string =>  {
-    return (`allEmails:${userId}`);
-} 
+const getAllEmailsKey = (userId: number): string => {
+  return `all-emails:${userId}`;
+};
 
-const getEmailDetailsKey = (emailId : string) : string => {
-    return (`getEmailDetails:${emailId}`);
+const getEmailDetailsKey = (emailId: string): string => {
+  return `get-email-details:${emailId}`;
+};
+
+/** ApiKey Keys */
+const getApiKeyDetailsKey = (apiKeyId: string): string => {
+  return `api-key-details:${apiKeyId}`;
+};
+
+const getAllApiKeysKey = (userId: number): string => {
+  return `all-api-keys:${userId}`;
+};
+
+const getApiKeyLogsKey = (userId : number) : string => {
+  return `api-key-logs:${userId}`
+}
+
+const getApiKeyLogDetailsKey = (logId : string) : string => {
+  return `api-key-log-details:${logId}`
 }
 
 export {
-    getLoggedInUserKey,
-    getAllEmailsKey,
-    getEmailDetailsKey
-}
+  getLoggedInUserKey,
+  getAllEmailsKey,
+  getEmailDetailsKey,
+  getApiKeyDetailsKey,
+  getAllApiKeysKey,
+  getApiKeyLogsKey,
+  getApiKeyLogDetailsKey
+};
