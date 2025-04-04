7fb69d591adc67dc85e7561136f51b29ee3d6d09
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-04 12:52:34 +0530

    add redis

---
+++ apps/api/src/services/redis/index.ts
@@ -0,0 +1,20 @@
+import { createClient, type RedisClientType } from "redis";
+
+export const redis = createClient();
+
+async function connectRedis() {
+    redis.on('error', (err) => {
+        console.error("Redis client error", err);
+    });
+
+    redis.on('ready', () => {
+        console.log('Redis client connected');
+    });
+
+    await redis.connect();
+
+    await redis.ping();
+};
+
+
+export {connectRedis}
