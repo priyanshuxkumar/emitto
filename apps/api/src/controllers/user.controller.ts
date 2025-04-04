0569324182ad8c7ed98e8689a534207eb4b307b1
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-04 14:01:05 +0530

    add redis cache in user routes

--- apps/api/src/controllers/user.controller.ts
+++ apps/api/src/controllers/user.controller.ts
@@ -4,10 +4,31 @@ import { UpdateUserDetailsSchema } from '../types';
 import { ApiError } from '../utils/ApiError';
 import { HTTP_RESPONSE_CODE } from '../constants/constant';
 import { ApiResponse } from '../utils/ApiResponse';
+import { redis } from '../services/redis/index';
+import { getLoggedInUserKey } from '../services/redis/keys';
 
 const getUser = async(req: Request , res: Response, next: NextFunction) =>  {
     const userId = req.id as number;
     try {
+        let userKey = getLoggedInUserKey(userId as number); 
+
+        const cache = await redis.hGetAll(userKey);
+        if(cache && Object.keys(cache).length > 0) {
+            console.log(`Cache hit for ${req.baseUrl}${req.path}`);
+            res.status(HTTP_RESPONSE_CODE.SUCCESS).json(
+                new ApiResponse(
+                    true,
+                    HTTP_RESPONSE_CODE.SUCCESS,
+                    {
+                        id: Number(cache.id),
+                        email: cache.email,
+                        userMetadata: JSON.parse(cache.userMetadata as string),
+                    }
+                )
+            );
+            return;
+        }
+
         const user : User | null = await prisma.user.findFirst({
             where : {
                 id : userId
@@ -18,20 +39,29 @@ const getUser = async(req: Request , res: Response, next: NextFunction) =>  {
             throw new ApiError(false, HTTP_RESPONSE_CODE.NOT_FOUND, "User not found");
         }
 
+        const data = {
+            id: user.id,
+            email: user.email,
+            userMetadata: JSON.stringify({
+                name: user.name,
+                avatarUrl: user.avatarUrl,
+                email: user.email,
+                emailVerified: user.verified
+            })
+        }
+
+        await redis.hSet(userKey, data);
+
+        console.log(`Cache miss for ${req.baseUrl}${req.path}`);
         res.status(HTTP_RESPONSE_CODE.SUCCESS).json(
             new ApiResponse(
                 true,
                 HTTP_RESPONSE_CODE.SUCCESS,
                 {
                     id: user.id,
-                    email : user.email,
-                    userMetadata : {
-                        name : user.name,
-                        avatarUrl: user.avatarUrl,
-                        email : user.email,
-                        emailVerified: user.verified
-                    }
-                },
+                    email: user.email,
+                    userMetadata: JSON.parse(data.userMetadata),
+                }
             )
         );
     } catch (error : unknown) {
@@ -56,6 +86,19 @@ const updateUserDetails = async(req: Request , res: Response, next: NextFunction
                 ...parsedData.data
             }
         });
+
+        //Update the cache of user on redis 
+        let userKey = getLoggedInUserKey(userId);
+        await redis.hSet(userKey,{
+            id: updatedUser.id,
+            email: updatedUser.email,
+            userMetadata: JSON.stringify({
+                name: updatedUser.name,
+                avatarUrl: updatedUser.avatarUrl,
+                email: updatedUser.email,
+                emailVerified: updatedUser.verified
+            })
+        })
 
         res.status(HTTP_RESPONSE_CODE.SUCCESS).json(
             new ApiResponse(
