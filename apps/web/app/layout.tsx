c42b12ebe0debe5736461df994a7f58ff4d95fca
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-10 00:06:18 +0530

    remove user-provider

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner"
-import { UserProvider } from "@/context/userContext";
+import { Toaster } from "@/components/ui/sonner";
 
 
 const montserrat = Montserrat({
@@ -32,14 +31,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <UserProvider>
         <body
           className={`${montserrat.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
           <Toaster />
         </body>
-      </UserProvider>
     </html>
   );
 }
