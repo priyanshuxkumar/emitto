31d59be17b71a23612e6ac1b1224bf0095f72d2e
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-10 00:07:00 +0530

    add user-provider

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,25 +1,28 @@
 import Feedback from "@/components/Feedback";
 import Sidebar from "@/components/Sidebar";
+import { UserProvider } from "@/context/userContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
-      {/* Sidebar */}
-      <nav>
-        <aside className="w-64 flex flex-col justify-between h-screen border-r border-slate-6 p-4">
-          <Sidebar />
-        </aside>
-      </nav>
-
-      {/* Main Content */}
-      <main className="w-full">
-        <nav className="border-b h-15 border-slate-6 flex items-center justify-end px-6">
-          <Feedback/>
+    <UserProvider>
+      <div className="flex">
+        {/* Sidebar */}
+        <nav>
+          <aside className="w-64 flex flex-col justify-between h-screen border-r border-slate-6 p-4">
+            <Sidebar />
+          </aside>
         </nav>
-        <div className="h-[calc(100vh-60px)] overflow-auto">
-        {children}
-        </div>
-      </main>
-    </div>
+
+        {/* Main Content */}
+        <main className="w-full">
+          <nav className="border-b h-15 border-slate-6 flex items-center justify-end px-6">
+            <Feedback/>
+          </nav>
+          <div className="h-[calc(100vh-60px)] overflow-auto">
+          {children}
+          </div>
+        </main>
+      </div>
+    </UserProvider>
   );
 }
