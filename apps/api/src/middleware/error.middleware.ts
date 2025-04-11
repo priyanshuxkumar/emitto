22ae7dac63cc16783480077deca12c95fdb9abb0
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-04-11 19:10:12 +0530

    refactor error middleware

--- apps/api/src/middleware/error.middleware.ts
+++ apps/api/src/middleware/error.middleware.ts
@@ -11,46 +11,69 @@ export const errorMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  console.error(`Error occurred: ${req.method} ${req.path}`, err);
+  console.error(`Error occurred: ${req.method} ${req.path} ${err}`);
 
+  //Custom error
   if (err instanceof ApiError) {
     res
       .status(err.statusCode)
       .json({ success: err.success, message: err.message });
     return;
-  } 
-  else if (err instanceof Error) {
-    res
-      .status(HTTP_RESPONSE_CODE.SERVER_ERROR)
-      .json({ success: false, message: err.message });
-    return;
-  } 
-  else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+  }
+
+  // Prisma Errors 
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
     if (err.code === "P2025") {
       res
         .status(HTTP_RESPONSE_CODE.NOT_FOUND)
         .json({ success: false, message: "Data not found" });
       return;
+    }else if (err.code === "P2002") {
+      res.status(HTTP_RESPONSE_CODE.CONFLICT).json({
+        success: false,
+        message: "Data already exists",
+      });
+      return;
+    } else if (err.code === "P2012") {
+      res.status(HTTP_RESPONSE_CODE.BAD_REQUEST).json({
+        success: false,
+        message: "Missing required fields",
+      });
+      return;
     }
     res.status(HTTP_RESPONSE_CODE.SERVER_ERROR).json({
       success: false,
-      message: "Database error",
+      message: "Something went wrong",
+      code: err.code,
     });
     return;
   } 
-  else if (err instanceof ZodError) {
+
+  // Zod validation error
+  if (err instanceof ZodError) {
     res.status(HTTP_RESPONSE_CODE.BAD_REQUEST).json({
       success: false,
       message: err?.issues[0]?.message || "Invalid input",
     });
     return;
   } 
-  else if (err instanceof JsonWebTokenError) {
+
+  // JWT error
+  if (err instanceof JsonWebTokenError) {
     res
       .status(HTTP_RESPONSE_CODE.UNAUTHORIZED)
       .json({ success: false, message: "Unauthorized access" });
     return;
-  } 
+  }
+
+  // General error
+  if (err instanceof Error) {
+    res
+      .status(HTTP_RESPONSE_CODE.SERVER_ERROR)
+      .json({ success: false, message: err.message });
+    return;
+  }
+
   else {
     res
       .status(HTTP_RESPONSE_CODE.SERVER_ERROR)
